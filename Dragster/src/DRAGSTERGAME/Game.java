aa644cf254e2b43deb312d13e4a52aac5b036820
Archie Byrne <mabyrne25@gmail.com>
2022-05-29 00:57:31 -0700

    Backend begun, jframe window in place

---
+++ Dragster/src/DRAGSTERGAME/Game.java
@@ -0,0 +1,36 @@
+package DRAGSTERGAME;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+    private Thread thread;
+    private boolean running = false;
+
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, "Dragster", this);
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+    public synchronized void stop() {
+    try {
+        thread.join();
+        running = false;
+    }catch(Exception e) {
+        e.printStackTrace();
+    }
+    }
+    public void run() {
+
+    }
+    public static void main(String[] args) {
+        new Game();
+    }
+}
